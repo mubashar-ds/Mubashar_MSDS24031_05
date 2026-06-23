ec0eccc333227cffbdf650fa75cdc423532a9bf8
Mubashar Hussain <mubashar.cui@gmail.com>
2026-06-24 04:33:54 +0500

    implemented encoder and decoder blocks

--- codes/model.py
+++ codes/model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 
-
 class DoubleConv(nn.Module):
 
     def __init__(self, in_channels, out_channels):
@@ -17,19 +16,52 @@ class DoubleConv(nn.Module):
         )
 
     def forward(self, x):
-        
+
         return self.layers(x)
+class DownBlock(nn.Module):
+
+    def __init__(self, in_channels,out_channels):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+
+        return self.layers(x)
+class UpBlock(nn.Module):
+
+    def __init__(self,in_channels, out_channels):
+        super().__init__()
+
+        self.up = nn.ConvTranspose2d(in_channels,out_channels, kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x, skip):
+
+        x = self.up(x)
+        x = torch.cat([skip,x], dim=1)
+        x = self.conv(x)
+
+        return x
 
 if __name__ == '__main__':
 
     x = torch.randn(2, 3, 64, 64)
 
-    print('\ninput shape')
-    print(x.shape)
+    print('\ninput shape:',x.shape)
 
     double_conv = DoubleConv(3, 64)
     y = double_conv(x)
+    print('\nafter DoubleConv:', y.shape)
 
-    print('\nafter DoubleConv')
-    print(y.shape)
+    down = DownBlock(64, 128)
+    y_down= down(y)
+    print('after DownBlock:', y_down.shape)
+
+    up = UpBlock(128, 64)
+    y_up = up(y_down,y)
+    print('after UpBlock;', y_up.shape)
 
