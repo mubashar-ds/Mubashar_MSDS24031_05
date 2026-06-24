381fbdaa30d754debcc67eac69db403633630370
Mubashar Hussain <mubashar.cui@gmail.com>
2026-06-24 10:11:07 +0500

    implemented diffusion unet for denoising

--- codes/model.py
+++ codes/model.py
@@ -64,6 +64,54 @@ class TimeEmbedding(nn.Module):
         t = t.float().unsqueeze(1)
 
         return self.embedding(t)
+    
+class DiffusionUNet(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        # time embedding...
+        self.time_embedding = TimeEmbedding(256)
+
+        # encoder..
+        self.input_conv = DoubleConv(3,64)
+        self.down1 = DownBlock(64, 128)
+        self.down2 = DownBlock(128, 256)
+
+        # bottleneck..
+        self.bottleneck = DoubleConv(256,256)
+
+        # decoder..
+        self.up1 = UpBlock(256, 128)
+        self.up2 = UpBlock(128,64)
+
+        # final output..
+        self.output_conv = nn.Conv2d(64, 3,kernel_size=1)
+
+    def forward(self, x, t):
+
+        # time embedding..
+        t = self.time_embedding(t)
+
+        # encoder
+        skip1 = self.input_conv(x)
+        skip2 = self.down1(skip1)
+        x = self.down2(skip2)
+
+        # bottleneck
+        x =self.bottleneck(x)
+
+        # injecting timestep information..
+        t = t.unsqueeze(-1).unsqueeze(-1)
+
+        x = x + t
+
+        # decoder
+        x = self.up1(x,skip2)
+        x = self.up2(x, skip1)
+        x = self.output_conv(x)
+
+        return x
 
 if __name__ == '__main__':
 
@@ -87,4 +135,16 @@ if __name__ == '__main__':
 
     t = torch.tensor([10, 500])
     emb = time_embedding(t)
-    print('\ntime Embedding:', emb.shape, '\n')
+    print('time Embedding:', emb.shape)
+
+    x = torch.randn(2, 3, 64, 64)
+    t = torch.tensor([100, 700])
+
+    model = DiffusionUNet()
+
+    output = model(x, t)
+
+    print('\ntesting diffusion UNet..')
+
+    print(f'input Shape : {x.shape}')
+    print(f'output Shape : {output.shape}\n')
