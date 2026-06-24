d0d21564232bcc14d97fb8a3a056a0a5094c7689
Mubashar Hussain <mubashar.cui@gmail.com>
2026-06-25 04:37:16 +0500

    normalized generated images before output

--- codes/diffusion.py
+++ codes/diffusion.py
@@ -53,6 +53,8 @@ class Diffusion:
             x = (1 / torch.sqrt(alpha)) * (x - ((1 - alpha) / torch.sqrt(1 - alpha_hat)) * predicted_noise) + torch.sqrt(beta) * noise
 
         model.train()
+        
+        x = (x.clamp(-1, 1) + 1) / 2
 
         return x
 
